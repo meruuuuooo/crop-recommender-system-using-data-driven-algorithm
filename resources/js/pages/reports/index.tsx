cac9a4281e5b22dd7236580fd37e7f02c63a6cf6
Ben-hur <mailem.meldavid@gmail.com>
2025-08-27 17:22:45 +0800

    update breadcrumbs and page title to reflect Reports section

--- resources/js/pages/reports/index.tsx
+++ resources/js/pages/reports/index.tsx
@@ -6,15 +6,15 @@ import { Head } from '@inertiajs/react';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
-        title: 'Dashboard',
-        href: '/dashboard',
+        title: 'Reports',
+        href: '/reports/index',
     },
 ];
 
 export default function Index() {
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
-            <Head title="Dashboard" />
+            <Head title="Reports" />
             <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-8 overflow-x-auto">
 
                 <Card className="relative min-h-[100vh] flex-1 overflow-hidden rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border">
