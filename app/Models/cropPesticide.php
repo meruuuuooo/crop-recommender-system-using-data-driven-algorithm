8254329a39673c6d4851dc2108979bb51336e8a5
Ben-hur <mailem.meldavid@gmail.com>
2025-10-05 12:08:14 +0800

    asd

--- app/Models/cropPesticide.php
+++ app/Models/cropPesticide.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class cropPesticide extends Model
+class CropPesticide extends Model
 {
     use HasFactory;
 
